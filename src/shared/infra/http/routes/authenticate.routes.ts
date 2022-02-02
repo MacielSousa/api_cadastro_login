5dc988f02cbf2ff84be95edb3f60eb0a423967d8
unknown <fmacielsousa@gmail.com>
2022-02-02 04:22:07 -0300

    feat: refresh token

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { AthenticateUserUseController } from "@modules/accounts/userCase/authenticateUser/AthenticateUserUseController";
+import { RefreshTokenController } from "@modules/accounts/userCase/refreshToken/RefreshTokenController";
 import { Router } from "express";
 
 
 const authenticateRoutes = Router();
 
+const refreshTokenController = new RefreshTokenController();
 const authenticateUserController = new AthenticateUserUseController();
 
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticateRoutes }
